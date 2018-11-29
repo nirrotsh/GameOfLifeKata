067bc1d5b50b6b5dd31658053037c81f899bd2d9
Nir Rotshtein <nirr@qognify.com>
2018-11-29 09:07:12 +0200

    Added unit tests for game rules

--- python-3/test_gameoflife.py
+++ python-3/test_gameoflife.py
@@ -16,3 +16,30 @@ def test_sample_next_gen():
     zeroGen = init(4,8, [(2,4),(3,4),(3,5)])
     nGen = nextGen(zeroGen)
     assert exp == nGen
+
+def test_live_cells_with_less_then_two_neighbors_should_die():
+    zeroGen = init(4,8, [(2,2), (2,7),(3,6)])
+    fGen = nextGen(zeroGen)
+    assert fGen[1][1] == 0
+    assert fGen[1][6] == 0
+
+def test_live_cells_with_more_then_three_neighbors_should_die():
+    zeroGen = init(4,8, [(1,5), (2,3),(2,4),(2,5),(3,3),(3,4)])
+    fGen = nextGen(zeroGen)
+    assert fGen[2][3] == 0
+    assert fGen[1][3] == 0
+
+def test_live_cells_with_two_live_neighbors_should_stay_alive():
+    zeroGen = init(4,8, [(2,1), (3,1),(3,2)])
+    fGen = nextGen(zeroGen)
+    assert fGen[2][0] == 1
+
+def test_dead_cells_with_exactly_three_live_neighbors_should_become_alive():
+    zeroGen = init(6,6, [(1,2), (2,1),(2,2)])
+    fGen = nextGen(zeroGen)
+    assert fGen[0][0] == 1
+
+def test_dead_cells_with_more_than_three_live_neighbors_should_stay_dead():
+    zeroGen = init(6,6, [(1,1), (1,3),(2,1),(2,2),(2,3)])
+    fGen = nextGen(zeroGen)
+    assert fGen[0][1] == 0
